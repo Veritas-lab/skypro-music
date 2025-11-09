239cda6973b568eab5efed967f29023f17471dce
Veritas-lab <veritas91@list.ru>
2025-11-10 00:13:15 +0300

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import "./page.css";
+import styles from "./page.module.css";
+import Bar from "@/components/Bar/Bar";
+import Centerblock from "@/components/Centerblock/Centerblock";
+import Navigation from "@/components/Navigation/Navigation";
+import Sidebar from "@/components/Sidebar/Sidebar";
+
+export default function Home() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Navigation />
+          <Centerblock />
+          <Sidebar />
+        </main>
+        <Bar />
+        <footer className="footer"></footer>
+      </div>
+    </div>
+  );
+}
