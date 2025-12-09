c44f3ea20078b0baba3c1246d1440978dd9aeb57
Veritas-lab <veritas91@list.ru>
2025-12-09 21:37:28 +0300

    Переименован файл

--- src/Store/ReduxProvider.tsx
+++ src/Store/ReduxProvider.tsx
@@ -2,7 +2,7 @@
 
 import { useMemo } from "react";
 import { Provider } from "react-redux";
-import { makeStore } from "./store";
+import { makeStore } from "./Store";
 
 export default function ReduxProvider({
   children,
